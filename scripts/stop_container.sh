69a20d0e48910f38433afb857390213e72cc7676
kandulavk8 <139133315+kandulavk8@users.noreply.github.com>
2024-07-07 00:29:50 +0530

    vk

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-sudo containerid=`docker ps | awk -F " " '{print $1}'`
+containerid=`sudo docker ps | awk -F " " '{print $1}'`
 sudo docker rm -f $containerid
